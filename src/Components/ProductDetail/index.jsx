7aba4adf618ee63b52ea1da747f7bc5047d6cecb
Julián Touza <jtouza@bancopatagonia.com.ar>
2023-06-02 22:56:53 -0300

    Product detail

---
+++ src/Components/ProductDetail/index.jsx
@@ -0,0 +1,12 @@
+const ProductDetail = () => {
+    return (
+        <aside className='flex flex-col bg-white border border-black rounded-lg  w-[360px] fixed right-0 top-[68px] h-[calc(100vh-68px)]'>
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'>Title</h2>
+                <span>x</span>
+            </div>
+        </aside>
+    )
+}
+
+export default ProductDetail
