4262b48f8601ee356c28d40e77f0e1c343d7c3ce
Julián Touza <jtouza@bancopatagonia.com.ar>
2023-06-16 00:50:39 -0300

    My orders

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,11 +1,30 @@
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Components/Context'
 import Layout from '../../Components/Layout'
+import OrdersCard from '../../Components/OrdersCard'
+
 
 function MyOrders() {
-    return (
-      <Layout>
-        MyOrders
-      </Layout>
-    )
-  }
-  
-  export default MyOrders
+
+  const { order } = useContext(ShoppingCartContext)
+
+  return (
+    <Layout>
+      MyOrders
+      <div>
+        {
+          order.map((order, index) => (
+            <Link key={index} to={`/my-orders/${index}`}>
+              <OrdersCard
+                order={order}
+              />
+            </Link>
+          ))
+        }
+      </div>
+    </Layout>
+  )
+}
+
+export default MyOrders
